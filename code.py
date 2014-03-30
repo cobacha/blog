676f10e1ec729606d3b0309996b48fc31495cc5d
cobacha <xiaoxinchen1989@gmail.com>
2014-03-30 21:14:07 +0800

    fix admin authority bug
    add admin authority checker

--- code.py
+++ code.py
@@ -8,9 +8,10 @@ import ConfigParser
 import Post
 import re
 import HTMLParser
+from BaseHandler import BaseHandler
 
-config = ConfigParser.SafeConfigParser({'email': 'cabocha@163.com', 'password': '123456'})
-config.read('admin.cfg')
+blogconfig = ConfigParser.SafeConfigParser({'email': 'cabocha@163.com', 'password': '123456'})
+blogconfig.read('blog.cfg')
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -28,8 +29,8 @@ def stripTags(html):
     return "".join(result)
 
 class Index(webapp2.RequestHandler):
-    def get(self):
-        posts=Post.List()         
+    def get(self):        
+        posts=Post.List()        
         ps=[]
         for post in posts:                                    
             p={}
@@ -47,26 +48,31 @@ class View(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('template/post.html')
         self.response.write(template.render({"post":post}))            
 
-class Login(webapp2.RequestHandler):
+class Login(BaseHandler):
     def get(self):        
         template = JINJA_ENVIRONMENT.get_template('template/login.html')
         self.response.write(template.render())
     def post(self):        
         email=self.request.get('email')
         password=self.request.get('password')            
-        if config.get('admin', 'email')==email and config.get('admin', 'password')==password:            
+        if blogconfig.get('admin', 'email')==email and blogconfig.get('admin', 'password')==password:            
+            self.session['admin'] = 'logged'
             self.redirect('/admin')
         else:
             self.response.write("ERROR")
 
-class Admin(webapp2.RequestHandler):
-    def get(self):        
+class Admin(BaseHandler):
+    def get(self):       
+        if (self.session.get('admin', '')!='logged'):
+            self.redirect('/login')
         posts=Post.List()
         template = JINJA_ENVIRONMENT.get_template('template/admin.html')
         self.response.write(template.render({"posts":posts}))     
 
-class Edit(webapp2.RequestHandler):
+class Edit(BaseHandler):
     def get(self):        
+        if (self.session.get('admin', '')!='logged'):
+            self.redirect('/login')
         p=None
         key=self.request.get("id")        
         if key!=None and key!="":
@@ -74,8 +80,10 @@ class Edit(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('template/edit.html')
         self.response.write(template.render({"post":p}))     
 
-class Save(webapp2.RequestHandler):
+class Save(BaseHandler):
     def post(self):       
+        if (self.session.get('admin', '')!='logged'):
+            self.redirect('/login')
         key=self.request.get("key")
         title=self.request.get("title")
         content=self.request.get("content")
@@ -83,11 +91,20 @@ class Save(webapp2.RequestHandler):
         Post.SavePost(key,title,content)
         self.response.write("SUCCESS")
 
-class Delete(webapp2.RequestHandler):
+class Delete(BaseHandler):
     def get(self):        
+        if (self.session.get('admin', '')!='logged'):
+            self.redirect('/login')
         key=self.request.get("id")
         Post.Delete(key)
         self.redirect('/admin')      
+
+
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'cabocha',
+}
+
 
 application = webapp2.WSGIApplication([
     ('/', Index),        
@@ -97,4 +114,4 @@ application = webapp2.WSGIApplication([
     ('/edit', Edit),
     ('/save', Save),
     ('/del', Delete),
-], debug=True)
+], config=config, debug=True)
